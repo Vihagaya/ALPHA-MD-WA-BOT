4ff562e702f2339738dcc87e570f00eddc45b951
Vihagaya <114232657+Vihagaya@users.noreply.github.com>
2022-09-24 15:15:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
+global.owner = ['+94703103938']
+global.premium = ['+94703103938']
+global.ownernomer = '+94703103938'
 global.ownername = 'Achi Fernando'
 global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
